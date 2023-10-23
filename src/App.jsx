d1bf6e883f4726bbb208ce697b292572896dc040
thakursachin05 <sachinrajput10101010@gmail.com>
2023-10-23 22:59:05 +0530

    Update router

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
+import { HashRouter } from 'react-router-dom';
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
           <Route exact path="/" element= {  <Home /> }/>
-          <Route  path="/login" element= {  <Login/> }/>
+          <Route path="/login" element= {  <Login/> }/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 };
