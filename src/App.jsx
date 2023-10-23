f2fa1b538dfed1f7d582fc013697ef4c51b146b6
thakursachin05 <sachinrajput10101010@gmail.com>
2023-10-23 23:01:11 +0530

    Update hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, HashRouter, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
-import { HashRouter } from 'react-router-dom';
 const App = () => {
   return (
     <HashRouter>
