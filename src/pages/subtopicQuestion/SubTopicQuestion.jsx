feda2b044970df020807b52a30ce2e5c16368342
Ashutosh741 <aashutoshmishra296@gmail.com>
2023-11-01 10:59:21 +0530

    error page css

--- src/pages/subtopicQuestion/SubTopicQuestion.jsx
+++ src/pages/subtopicQuestion/SubTopicQuestion.jsx
@@ -3,11 +3,15 @@ import Header from "../../component/header/Header";
 import Footer from "../../component/footer/Footer";
 import BreadCrumbBanner from "../../component/breadcrumb/BreadCrumbBanner";
 import Question from "../../component/questions/Question";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import Nopage from "../nopage/Nopage";
 
 const SubTopicQuestion = () => {
   const { topic } = useParams();
+  const notAllowed = ["login","register","forgotpassword","signup"]
+  if(topic.includes(notAllowed)){
+    return <Navigate to = "/"/>
+  }
   const allowedTopics = ["QUANT", "DI", "LR", "VARC"];
 
   if (!allowedTopics.includes(topic)) {
--- src/pages/subtopicslist/SubTopicsList.jsx
+++ src/pages/subtopicslist/SubTopicsList.jsx
@@ -3,11 +3,15 @@ import Header from "../../component/header/Header";
 import BreadCrumbBanner from "../../component/breadcrumb/BreadCrumbBanner";
 import Footer from "../../component/footer/Footer";
 import SubTopics from "../../component/subtopics/SubTopics";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import Nopage from "../nopage/Nopage";
 
 const SubTopicsList = () => {
   const { topic } = useParams();
+  const notAllowed = ["login","register","forgotpassword","signup"]
+  if(topic.includes(notAllowed)){
+    return <Navigate to = "/"/>
+  }
   const allowedTopics = ["QUANT", "DI", "LR", "VARC"];
   if (!allowedTopics.includes(topic)) {
     return <Nopage />;
