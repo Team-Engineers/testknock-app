bcb5ef5c00c75f0a68c35c52296fbbab93b76ddd
thakursachin05 <sachinrajput10101010@gmail.com>
2023-11-13 14:31:36 +0530

    fixed mathjax issue

---
+++ src/component/mathJax/MathText.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MathComponent } from 'mathjax-react';
+export const MathText = ({ text, textTag = 'p' }) => {
+  const parts = text.split(/\$(.*?)\$/);
+  const TextTag = textTag || 'p';
+
+  const jsxElements = parts.map((part, index) => {
+    if (index % 2 === 0) {
+      return <span key={index}>{part}</span>;
+    } else {
+      return <MathComponent key={index} tex={part} display={false}/>;
+    }
+  });
+
+  return <TextTag className='text-spacing'>{jsxElements}</TextTag>;
+};
+
+
