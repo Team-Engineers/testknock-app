b0f21248cacc97526ebbaf7b7292adb7bec803aa
yojana-tripathi <96787086+yojana-tripathi@users.noreply.github.com>
2023-11-16 23:46:48 +0530

    Feat-changetheimages (#52)
    * subtopic link error solve
    
    * change the logo of subtopics
    
    * minor change in images
    
    * minor changes in links
    
    * change the border of subtopics
    
    * 123
    
    ---------
    
    Co-authored-by: Ashutosh <106524453+Ashutosh741@users.noreply.github.com>

--- src/component/subtopics/SubTopics.jsx
+++ src/component/subtopics/SubTopics.jsx
@@ -2,53 +2,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../../assets/images/subtopic-logo.png";
 import { subtopicsData } from "../../utils/constants";
+import { useParams } from "react-router-dom";
 
 const TopicCard = styled.li`
-  height: 6rem;
-  background-color: white;
-  border-radius: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: dashed;
   text-align: center;
-  width: 18rem;
-
+  height: 11rem;
+  width: 13rem;
+  border: 1px solid #bababa;
+  border-radius: 10px;
   &:hover {
+    box-shadow: 1px 1px 1px 1px #eacfcc;
+  }
+
+   {
     background: blueviolet;
-    color: white;
   }
   &:hover a {
     color: white;
   }
   img {
-    height: 20px;
-    width: 20px;
+    width: 6rem;
+    height: 6rem;
+    object-fit: cover;
   }
 `;
 
 const Wrapper = styled.ul`
-  width: fit-content;
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-wrap: wrap;
   gap: 2rem;
+  padding: 0px;
 `;
 
 const MarginTop = styled.div`
-  margin-top: 1rem;
+  margin-top: 2rem;
 `;
 
 const Box = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
 const Box1 = styled.div`
-  width: 3rem;
+  width: 6rem;
 `;
 const Box2 = styled.h6`
   width: 12rem;
@@ -57,8 +62,11 @@ const Box2 = styled.h6`
   overflow-wrap: break-word;
 `;
 
+//
 const SubTopics = (props) => {
   const selectedSubTopics = subtopicsData[props.topic] || [];
+  let imageIndex = 1;
+  const { topic } = useParams();
 
   return (
     <div className="container">
@@ -66,29 +74,50 @@ const SubTopics = (props) => {
         <div className="col">
           <MarginTop>
             <h2 className="fs-5 fw-bolder mb-5">Subtopics</h2>
+
             <Wrapper>
-              {selectedSubTopics.map((subtopic, index) => (
-                <Link
-                  to={`/${encodeURIComponent(props.topic)}/${subtopic.replace(
-                    /\s/g,
-                    "_"
-                  )}`}
-                  key={index}
-                >
-                  <TopicCard>
-                    <Box>
-                      <Box1>
-                        <img
-                          src={logo}
-                          alt="subtopic-logo"
-                          className="img-fluid"
-                        />
-                      </Box1>
-                      <Box2>{subtopic}</Box2>
-                    </Box>
-                  </TopicCard>
-                </Link>
-              ))}
+              {selectedSubTopics.map((subtopic, index) => {
+                let imagePath;
+
+                if (topic === "LOGICAL_REASONING") {
+                  imagePath = `${imageIndex + 12}.jpg`;
+                } else if (topic === "DATA_INTERPRETATION") {
+                  const adjustedIndex = imageIndex % 31;
+                  imagePath = `${adjustedIndex}.jpg`;
+                } else if (
+                  topic === "VERBAL_ABILITY_AND_READING_COMPREHENSION"
+                ) {
+                  imagePath = `${imageIndex + 21}.jpg`;
+                } else {
+                  imagePath = `${imageIndex + 1}.jpg`;
+                }
+
+                imageIndex++;
+                imageIndex = imageIndex % 31;
+
+                return (
+                  <Link
+                    to={`/${encodeURIComponent(props.topic)}/${subtopic.replace(
+                      /\s/g,
+                      "_"
+                    )}`}
+                    key={index}
+                  >
+                    <TopicCard>
+                      <Box>
+                        <Box1>
+                          <img
+                            src={require(`../../assets/images/subTopicLogo/${imagePath}`)}
+                            alt="subtopic-logo"
+                            className="img-fluid"
+                          />
+                        </Box1>
+                        <Box2>{subtopic}</Box2>
+                      </Box>
+                    </TopicCard>
+                  </Link>
+                );
+              })}
             </Wrapper>
           </MarginTop>
         </div>
