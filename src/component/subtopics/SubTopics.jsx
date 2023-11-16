cf0558bb5bc227cf6f242aaa625f542bdc035154
Yojana <tripathiyojana640@gmail.com>
2023-11-16 17:44:44 +0530

    change the logo of subtopics

--- src/component/subtopics/SubTopics.jsx
+++ src/component/subtopics/SubTopics.jsx
@@ -1,20 +1,20 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../../assets/images/subtopic-logo.png";
 import { subtopicsData } from "../../utils/constants";
 
 const TopicCard = styled.li`
-  height: 6rem;
-  background-color: white;
-  border-radius: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: dashed;
   text-align: center;
-  width: 18rem;
+  height: 11rem;
+  width: 13rem;
+  border: dashed;
+  border-radius: 10px;
+  &:hover {
+    box-shadow: 1px 1px 1px 1px #eacfcc;
+  }
 
   &:hover {
     background: blueviolet;
@@ -24,41 +24,45 @@ const TopicCard = styled.li`
     color: white;
   }
   img {
-    height: 20px;
-    width: 20px;
+    width: 6rem;
+    height: 6rem;
+    object-fit: cover;
   }
 `;
 
 const Wrapper = styled.ul`
-  width: fit-content;
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-wrap: wrap;
   gap: 2rem;
+  padding: 0px;
 `;
 
 const MarginTop = styled.div`
-  margin-top: 1rem;
+  margin-top: 2rem;
 `;
 
 const Box = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
 const Box1 = styled.div`
-  width: 3rem;
+  width: 6rem;
 `;
 const Box2 = styled.h6`
   width: 12rem;
   margin-bottom: 0px;
   text-transform: uppercase;
-  overflow-wrap: break-word;
 `;
 
 const SubTopics = (props) => {
   const selectedSubTopics = subtopicsData[props.topic] || [];
+  let imageIndex = 1;
 
   return (
     <div className="container">
@@ -66,29 +70,35 @@ const SubTopics = (props) => {
         <div className="col">
           <MarginTop>
             <h2 className="fs-5 fw-bolder mb-5">Subtopics</h2>
+
             <Wrapper>
-              {selectedSubTopics.map((subtopic, index) => (
-                <Link
-                  to={`/${encodeURIComponent(props.topic)}/${subtopic.replace(
-                    /\s/g,
-                    "_"
-                  )}`}
-                  key={index}
-                >
-                  <TopicCard>
-                    <Box>
-                      <Box1>
-                        <img
-                          src={logo}
-                          alt="subtopic-logo"
-                          className="img-fluid"
-                        />
-                      </Box1>
-                      <Box2>{subtopic}</Box2>
-                    </Box>
-                  </TopicCard>
-                </Link>
-              ))}
+              {selectedSubTopics.map((subtopic, index) => {
+                const imagePath = `${imageIndex + 1}.jpg`; // Dynamically generate image path
+                imageIndex++;
+                imageIndex = imageIndex % 31;
+                return (
+                  <Link
+                    to={`/${encodeURIComponent(props.topic)}/${subtopic.replace(
+                      /\s/g,
+                      "_"
+                    )}`}
+                    key={index}
+                  >
+                    <TopicCard>
+                      <Box>
+                        <Box1>
+                          <img
+                            src={require(`../../assets/images/subTopicLogo/${imagePath}`)}
+                            alt="subtopic-logo"
+                            className="img-fluid"
+                          />
+                        </Box1>
+                        <Box2>{subtopic}</Box2>
+                      </Box>
+                    </TopicCard>
+                  </Link>
+                );
+              })}
             </Wrapper>
           </MarginTop>
         </div>
