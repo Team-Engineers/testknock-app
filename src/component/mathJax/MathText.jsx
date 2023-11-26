8483096f4d85884458c6af10732136d0b962f3c4
Ashutosh <106524453+Ashutosh741@users.noreply.github.com>
2023-11-26 22:35:58 +0530

    this is best way, to render math jx and html tag (#64)

--- src/component/mathJax/MathText.jsx
+++ src/component/mathJax/MathText.jsx
@@ -5,17 +5,24 @@ import './mathText.css';
 export const MathText = ({ text, textTag = 'p' }) => {
   const TextTag = textTag || 'p';
 
-  if(typeof text !== 'string'){
-    text = text[0]
+  if (typeof text !== 'string') {
+    text = text[0];
   }
-  
+
+  // Check if < > tags are present in the text
+  const hasHTMLTags = /<.*?>/.test(text);
+
+  if (hasHTMLTags) {
+    return <TextTag className='text-spacing' dangerouslySetInnerHTML={{ __html: text }} />;
+  }
+
   const parts = text.split(/\$(.*?)\$/);
 
   const jsxElements = parts.map((part, index) => {
     if (index % 2 === 0) {
       return <span key={index}>{part}</span>;
     } else {
-      return <MathComponent key={index} tex={part} display={false}  className='math-expression' />;
+      return <MathComponent key={index} tex={part} display={false} className='math-expression' />;
     }
   });
 
